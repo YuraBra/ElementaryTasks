2aab7d02b6dfd9ffe4f241ab36f99b0c8a33c56d
Yura <46863662+YuraBra@users.noreply.github.com>
2020-02-12 21:47:00 +0200

    uploaded via

---
+++ 1_Chess_board/src/com/elementary_tasks/the_first_one/ChessBoard.java
@@ -0,0 +1,39 @@
+package com.elementary_tasks.the_first_one;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+class ChessBoard {
+
+    private boolean success;
+    private static int height;
+    private static int width;
+
+    static void dataInput() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter height:");
+        height = scan.nextInt();
+        System.out.println("Enter width:");
+        width = scan.nextInt();
+    }
+
+    void calculation() {
+        while (!success) {
+            try {
+                ChessBoard.dataInput();
+                for (int j = 0; j < height; j++) {
+                    if ((j + 1) % 2 == 0) {
+                        System.out.print(" ");
+                    }
+                    for (int i = 0; i < width; i++) {
+                        System.out.print("* ");
+                    }
+                    System.out.println("");
+                    success = true;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Enter a positive integer");
+            }
+        }
+    }
+}
---
+++ 1_Chess_board/src/com/elementary_tasks/the_first_one/Main.java
@@ -0,0 +1,17 @@
+package com.elementary_tasks.the_first_one;
+
+/*1.	Шахматная доска
+Вывести шахматную доску с заданными размерами высоты и ширины, по принципу:
+*  *  *  *  *  *
+  *  *  *  *  *  *
+*  *  *  *  *  *
+  *  *  *  *  *  */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ChessBoard chessBoard = new ChessBoard();
+        chessBoard.calculation();
+    }
+}
